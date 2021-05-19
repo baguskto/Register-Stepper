5a24958baf6ff72fac3ff0cd90b40975334fc292
baguskto <baguskto@gmail.com>
2021-05-19 10:52:13 +0700

    feat: add register_stepper page

---
+++ lib/register_stepper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterStepper extends StatefulWidget {
+  @override
+  _RegisterStepperState createState() => _RegisterStepperState();
+}
+
+class _RegisterStepperState extends State<RegisterStepper> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
