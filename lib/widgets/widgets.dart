4b69dd5dd37d452257252a4087de9392ad4a62fa
baguskto <baguskto@gmail.com>
2021-05-20 10:26:25 +0700

    style: resgitering widgets

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -3,3 +3,5 @@ export 'widget_steper.dart';
 export 'widget_bg_email_path.dart';
 export 'widget_password_validation_item.dart';
 export 'widget_dropdown_field.dart';
+export 'widget_calender_animation.dart';
+export 'widget_schedule_field.dart';
