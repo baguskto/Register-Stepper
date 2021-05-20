7750e64f848c92daf5fcfc888814b2a720bd86c4
baguskto <baguskto@gmail.com>
2021-05-20 10:27:24 +0700

    feat: add time & date formater

--- lib/constant.dart
+++ lib/constant.dart
@@ -12,4 +12,24 @@ class Constant {
   static  List<String> personalExpensesOption = [
     '≤ Rp 5.000.000', '≤ Rp 10.000.000', '≥ Rp 20.000.000'
   ];
+
+  static  String dateFormatter(DateTime date) {
+    dynamic dayData =
+        '{ "1" : "Senin", "2" : "Selasa", "3" : "Rabu", "4" : "Kamis", "5" : "Jumat", "6" : "Sabtu", "7" : "Minggu" }';
+
+    dynamic monthData =
+        '{ "1" : "Jan", "2" : "Feb", "3" : "Mar", "4" : "Apr", "5" : "Mei", "6" : "Jun", "7" : "Jul", "8" : "Agu", "9" : "Sep", "10" : "Okt", "11" : "Nov", "12" : "Des" }';
+
+    return json.decode(dayData)['${date.weekday}'] +
+        ", " +
+        date.day.toString() +
+        " " +
+        json.decode(monthData)['${date.month}'] +
+        " " +
+        date.year.toString();
+  }
+
+  static String timeFormatter(DateTime time) {
+    return time.hour.toString() + ":" + time.minute.toString();
+  }
 }
