8c7c2153f2a16e26fd5dad204edbb4c184580831
baguskto <baguskto@gmail.com>
2021-05-20 10:16:05 +0700

    style: registering new widget

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'widget_button_next.dart';
 export 'widget_steper.dart';
 export 'widget_bg_email_path.dart';
 export 'widget_password_validation_item.dart';
+export 'widget_dropdown_field.dart';
