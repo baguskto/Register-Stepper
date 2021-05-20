c39f54bb3f15f832bf0669b66d0284a9306b7321
baguskto <baguskto@gmail.com>
2021-05-20 10:02:48 +0700

    feat: add password page

--- lib/pages/register_steps_page.dart
+++ lib/pages/register_steps_page.dart
@@ -13,6 +13,16 @@ class _RegisterStepsState extends State<RegisterSteps> {
   TextEditingController emailController = TextEditingController();
 
 
+  bool hidePassword = true;
+  TextEditingController passwordController = TextEditingController();
+  bool hasLowercase, hasUppercase, hasDigits, hasMoreThan9Characters = false;
+
+  Text complexity = Text(
+    "Very weak",
+    style: TextStyle(color: Colors.amberAccent),
+  );
+
+
   bool isValidEmail(input) {
     return RegExp(
         r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
@@ -165,6 +175,112 @@ class _RegisterStepsState extends State<RegisterSteps> {
                     ),
                   ],
                 )),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  Widget passwordPage() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+      height: MediaQuery.of(context).size.height / 1.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Create Password",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "Password will be used to login to account",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          TextFormField(
+            controller: passwordController,
+            keyboardType: TextInputType.visiblePassword,
+            obscureText: hidePassword,
+            decoration: InputDecoration(
+              fillColor: Colors.white,
+              filled: true,
+              contentPadding:
+              EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+              hintText: 'Create Password',
+              hintStyle: TextStyle(color: Colors.grey),
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    hidePassword = !hidePassword;
+                  });
+                },
+                color: Theme.of(context).focusColor,
+                icon: Icon(
+                    hidePassword ? Icons.visibility : Icons.visibility_off),
+              ),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: new BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.white)),
+              disabledBorder: InputBorder.none,
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: new BorderRadius.circular(10.0),
+                  borderSide: BorderSide(color: Colors.white)),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              Text(
+                "Complexity : ",
+                style: TextStyle(color: Colors.white),
+              ),
+              complexity,
+            ],
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              WidgetPasswordValidationItem(
+                title: "a",
+                isValid: hasLowercase == true,
+                label: "Lowercase",
+              ),
+              WidgetPasswordValidationItem(
+                title: "A",
+                isValid: hasUppercase == true,
+                label: "Uppercase",
+              ),
+              WidgetPasswordValidationItem(
+                title: "123",
+                isValid: hasDigits == true,
+                label: "Number",
+              ),
+              WidgetPasswordValidationItem(
+                title: "+9",
+                isValid: hasMoreThan9Characters == true,
+                label: "Caracters",
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
           ),
         ],
       ),
