b5c39f284bebf58b576d6edcb509cdc3d896b924
baguskto <baguskto@gmail.com>
2021-05-20 10:12:08 +0700

    feat: add constant data

---
+++ lib/constant.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+class Constant {
+
+  static  List<String> personalIncomeOption = [
+    '≤ Rp 5.000.000', '≤ Rp 10.000.000', '≥ Rp 20.000.000'
+
+  ];
+  static  List<String> personalGoalOption = [
+    'Life Style', 'Saving', 'Equity'
+  ];
+  static  List<String> personalExpensesOption = [
+    '≤ Rp 5.000.000', '≤ Rp 10.000.000', '≥ Rp 20.000.000'
+  ];
+}
